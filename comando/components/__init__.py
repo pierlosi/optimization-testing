0ed531777d4804f1846db6f09838ed148a0cf4d7
pierlosi <pierfrancesco@southernlights.io>
2023-12-07 09:50:07 +0100

    fix: package issues

--- comando/components/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Some example components for COMANDO"""
-# This file is part of the COMANDO project which is released under the MIT
-# license. See file LICENSE for full license details.
--- comando/examples/DESTEST/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Low Temperature District Heating Network Case Study"""
-# This file is part of the COMANDO project which is released under the MIT
-# license. See file LICENSE for full license details.
